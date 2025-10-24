55db0279ed9b1f035536f337672b1af3b6727b5a
Ziyuan Wu <zwuu0105@student.monash.edu>
2025-10-24 22:43:53 +1100

    fix vue CLI problem

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/fit5032-lab-efolios/'
-    : '/'
-}
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Use 'base' instead of 'publicPath'
+  base: process.env.NODE_ENV === 'production'
+    ? '/fit5032-lab-efolios/'  // Your repo name
+    : '/',
+  plugins: [vue()],
+})
