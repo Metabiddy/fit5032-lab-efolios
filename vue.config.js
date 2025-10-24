e02dbc55e02f6f65bd59871b74c82ffa12470944
Ziyuan Wu <zwuu0105@student.monash.edu>
2025-10-24 22:54:25 +1100

    vite.config

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Use 'base' instead of 'publicPath'
-  base: process.env.NODE_ENV === 'production'
-    ? '/fit5032-lab-efolios/'  // Your repo name
-    : '/',
+  // Force the base path directly.
+  base: '/fit5032-lab-efolios/',
   plugins: [vue()],
 })
